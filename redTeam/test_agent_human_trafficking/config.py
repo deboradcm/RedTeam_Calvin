bdfd6d3acac2f220ab998213acafebb2ca594c6a
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-11-19 17:16:23 -0400

    Ajustando o caminho para o arquivo .env

--- redTeam/test_agent_human_trafficking/config.py
+++ redTeam/test_agent_human_trafficking/config.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
-# Carregar variáveis do arquivo .env
-load_dotenv()
+# Caminho para o arquivo .env dentro da pasta 'agents'
+env_path = Path(__file__).resolve().parent / 'agents' / '.env'
+
+# Carregar variáveis do arquivo .env especificado
+load_dotenv(dotenv_path=env_path)
 
 # Recuperar variáveis de ambiente
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")

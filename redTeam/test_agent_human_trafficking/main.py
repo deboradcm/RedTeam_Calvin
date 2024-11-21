33b2e94237c015d0992c34fc8483e24d3e55ceaf
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-11-21 17:04:37 -0400

    Alterando arquivo principal para utilizar traffiking_agent_GPT4

--- redTeam/test_agent_human_trafficking/main.py
+++ redTeam/test_agent_human_trafficking/main.py
@@ -1,4 +1,4 @@
-from agents.trafficking_agent import conduct_conversation  # Importando a função principal do agente
+from agents.trafficking_agent_GPT4 import conduct_conversation_stream  # Importando a função principal do agente
 
 def main():
     print("Iniciando o agente de investigação de tráfico humano...")
@@ -7,7 +7,7 @@ def main():
     initial_input_text = "Oi, tudo bem?"
     
     # Inicia a conversa
-    conduct_conversation(initial_input_text)
+    conduct_conversation_stream(initial_input_text)
 
 if __name__ == "__main__":
     main()
